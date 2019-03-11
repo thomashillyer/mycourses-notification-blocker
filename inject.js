dd9a10c2f9f7ce23c0ddef8bd64f987ff8f14a60
Thomas Hillyer <thomashillyer@users.noreply.github.com>
2019-03-11 09:47:09 -0400

    add initial functions

---
+++ inject.js
@@ -0,0 +1,12 @@
+const notifBox = document.querySelector('.vjs-play-control');
+
+
+window.onload = hideNotifications;
+
+//add click listner to 'load more' button
+
+ window.addEventListener(/*'keydown'*/, hideNotifications);
+
+function hideNotifications() {
+
+}
