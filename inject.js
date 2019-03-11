725ffa1e8058d597f896434aff27e79e130fd227
Thomas Hillyer <thomashillyer@users.noreply.github.com>
2019-03-11 10:28:31 -0400

    add base selecting and temp highlighting

--- inject.js
+++ inject.js
@@ -1,12 +1,17 @@
-const notifBox = document.querySelector('.vjs-play-control');
+// <li> class = "d2l-datalist-item d2l-datalist-item-actionable d2l-datalist-simpleitem"
+const notifBox = document.querySelector('');
 
 
 window.onload = hideNotifications;
 
 //add click listner to 'load more' button
-
- window.addEventListener(/*'keydown'*/, hideNotifications);
+// window.addEventListener(/*'keydown'*/, hideNotifications);
 
 function hideNotifications() {
+	const osdItems = document.querySelectorAll("div[title^='Content Created, OSD']")
+
+	osdItems.forEach(function(divItem) {
+		divItem.style.backgroundColor = "red";
+	})
 
 }
