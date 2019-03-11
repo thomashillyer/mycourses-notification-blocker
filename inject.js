b14272758f24138f539c139169c6b222e523f869
Thomas Hillyer <thomashillyer@users.noreply.github.com>
2019-03-11 13:03:57 -0400

    remove notif working, loadMore functionality broken

--- inject.js
+++ inject.js
@@ -1,108 +1,76 @@
-// <li> class = "d2l-datalist-item d2l-datalist-item-actionable d2l-datalist-simpleitem"
-// const notifBox = document.querySelector('');
-
-
-// window.onload = hideNotifications;
-
-//add click listner to 'load more' button
-// window.addEventListener(/*'keydown'*/, hideNotifications);
-
-
-/*document.addEventListener('DOMContentLoaded', function(){
-	console.log("doc ready domCL");
-	updateButton = document.querySelector("button[title='Update alerts']");
-	updateButton.addEventListener('click', hideNotifications);
-}, false);*/
-/*let updateButton = null;
-window.addEventListener('load', ready, false );
-
-function ready() {
-	console.log("doc ready");
-
-	updateButton = document.querySelector("button[title='Update alerts']");
-	console.log(updateButton);
-	if(updateButton){
-		updateButton.addEventListener('click', hideNotifications);
-	}
-}*/
-
-
+let currVisNotifCount = 0;
 
 function hideNotifications() {
-	console.log("OSD notif blocker");
-	
-	const osdItems = document.querySelectorAll("div[title^='Content Created, OSD']")
-	// console.log(osdItems);
+    console.log("OSD notif blocker");
 
-	osdItems.forEach(function(divItem) {
-		let immedParent = divItem.parentElement;
-		let grandParent = immedParent.parentElement;
-		console.log(divItem.innerText);
-		grandParent.removeChild(immedParent);
-		divItem.style.backgroundColor = "red";
-	})
+    const osdItems = document.querySelectorAll("div[title^='Content Created, OSD']")
+    // console.log(osdItems);
+
+    osdItems.forEach(function(divItem) {
+        let immedParent = divItem.parentElement;
+        let grandParent = immedParent.parentElement;
+        console.log(divItem.innerText);
+        grandParent.removeChild(immedParent);
+        divItem.style.backgroundColor = "red";
+    })
 
 }
 
-function loadMore() {
+function countElem() {
+    console.log("it was clicked");
 
+    const notifBox = document.querySelector("div[id='AB_DL_PH_Grades']");
+    const list = notifBox.querySelector("ul[class='d2l-datalist vui-list']");
+    const notifCount = list.childElementCount;
+
+    const loadMoreButton = notifBox.querySelector("button[class*='d2l-loadmore-pager']");
+
+    console.log("new: " + notifCount);
+    console.log("old: " + currVisNotifCount);
+
+    if (notifCount == currVisNotifCount){
+        console.log(notifCount);
+        loadMoreButton.click();
+    	currVisNotifCount = notifBox.querySelector("ul[class='d2l-datalist vui-list']").childElementCount;
+    } else {
+    	//stop
+    }
 }
 
 
 // set up the mutation observer
-var observer = new MutationObserver(function (mutations, me) {
-  // `mutations` is an array of mutations that occurred
-  // `me` is the MutationObserver instance
-  /*const notifBox = document.querySelector("div[id='AB_DL_PH_Grades']");
-  if (notifBox) {
-    hideNotifications();
-    me.disconnect(); // stop observing
-    return;
-  }*/
+var observer = new MutationObserver(function(mutations, me) {
+    // `mutations` is an array of mutations that occurred
+    // `me` is the MutationObserver instance
 
-	const osdItems = document.querySelectorAll("div[title*='OSD note-taking page']")
-	// console.log(osdItems);
-	if(osdItems){
-		osdItems.forEach(function(divItem) {
-			let immedParent = divItem.parentElement;
-			let grandParent = immedParent.parentElement;
-			console.log(divItem.innerText);
-			grandParent.removeChild(immedParent);
-		})
-	}
+    // Delete all osd items whenever they appear
+    const osdItems = document.querySelectorAll("div[title*='OSD note-taking page']")
+    // console.log(osdItems);
+    if (osdItems) {
+        osdItems.forEach(function(divItem) {
+            let immedParent = divItem.parentElement;
+            let grandParent = immedParent.parentElement;
+            console.log(divItem.innerText);
+            grandParent.removeChild(immedParent);
+        })
+    }
+
+    // Click the load more button until something appears
+    const notifBox = document.querySelector("div[id='AB_DL_PH_Grades']");
+    if (notifBox) {
+    	// number of visible notifications after OSD deletion
+    	currVisNotifCount = notifBox.querySelector("ul[class='d2l-datalist vui-list']").childElementCount;
+        
+        const loadMoreButton = notifBox.querySelector("button[class*='d2l-loadmore-pager']");
+        loadMoreButton.addEventListener('click', countElem);
+        //me.disconnect(); // stop observing
+        //return;
+    }
 
 });
 
 // start observing
 observer.observe(document, {
-  childList: true,
-  subtree: true
+    childList: true,
+    subtree: true
 });
-
-
-
-
-/**********************************************/
-
-/*var mutationObserver = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-  	if(mutation.addedNodes.length > 0){
-  		console.log(mutation.addedNodes);
-  		if(mutation.addedNodes.firstElementChild){
-  			if(mutation.addedNodes.firstElementChild.title.substring(0,20) == "Content Created, OSD"){
-	    		
-	    		mutation.addedNodes.style.backgroundColor = "red";
-    		}	
-    	}
-	}
-  });
-});
-// Starts listening for changes in the root HTML element of the page.
-mutationObserver.observe(document.documentElement, {
-  attributes: true,
-  characterData: true,
-  childList: true,
-  subtree: true,
-  attributeOldValue: true,
-  characterDataOldValue: true
-});*/
